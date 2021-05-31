818edb783980ff01984f5dd11672074fac292714
Miguel A Santiago Jr <miguel@digiteck3d.com>
2021-05-30 23:52:23 -0500

    Batch file start program
    Adding in a simple batch file to execute for starting up the pancaketrade bot.

---
+++ StartPancakeBot.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ---------------------------------------------
+echo PancakeTrade : Starting Pancake Telegram Bot
+echo ---------------------------------------------
+rem call our python pip env and python script
+poetry run trade
